d9409a19b90c421a125223973e49f06f3945f7a1
David Conrad <david.scott.conrad@gmail.com>
2020-08-28 14:04:54 -0400

    Updates Current File
    Used process.cwd instead of ".". Same functionality. Improved cross platform compatablity.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log(message2);
 
 const fs = require('fs');
 
-fs.readdir('.', (err, filenames) => {
+fs.readdir(process.cwd(), (err, filenames) => {
     if(err) { //checks for error
         //throw new Error(err); Possible Solution
         console.log(err);
